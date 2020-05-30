a6d51c962e3ee7138dcdaa2ba820f03e002b6614
goelhoney <hemantgoel.4@gmail.com>
2020-05-30 13:46:49 +0530

    heroku changes

--- public/js/common.js
+++ public/js/common.js
@@ -8,7 +8,7 @@ form.addEventListener('submit', function(e) {
     message.textContent = 'Fetching....';
     e.preventDefault();
     const location = serach.value;
-    fetch('http://localhost:3000/weather?address='+location).then((response) => {
+    fetch('/weather?address='+location).then((response) => {
         response.json().then((data) => {
             message.textContent = data.Location;
         });
--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const hbs      = require('hbs');
 const geocode  = require('../utils/geocode');
 const forecast = require('../utils/forecast')
 
+const port = process.env.PORT || 3000;
 
 const app = new express();
 
@@ -94,6 +95,6 @@ app.get('*', (req, res) => {
     })
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Server started  at port 3000');
 });
